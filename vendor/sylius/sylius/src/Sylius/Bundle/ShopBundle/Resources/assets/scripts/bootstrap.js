b8d77b9b90931a89709592fbf36c209c4a485ad8
latelier22 <latelier.cyrille@gmail.com>
2025-01-26 22:08:14 +0000

    premier push

---
+++ vendor/sylius/sylius/src/Sylius/Bundle/ShopBundle/Resources/assets/scripts/bootstrap.js
@@ -0,0 +1,11 @@
+/*
+ * This file is part of the Sylius package.
+ *
+ * (c) Sylius Sp. z o.o.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap';
